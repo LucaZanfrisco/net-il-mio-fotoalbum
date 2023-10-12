d1088e52ce65ebe50920c774502e678ab80c5b58
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-10-12 09:36:20 +0200

    Implementazione della Modifica di una foto

--- net-il-mio-fotoalbum/Controllers/PhotoController.cs
+++ net-il-mio-fotoalbum/Controllers/PhotoController.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.SqlServer.Server;
 using net_il_mio_fotoalbum.Database;
 using net_il_mio_fotoalbum.Models;
+using NuGet.Protocol.Plugins;
 using System.Runtime.ConstrainedExecution;
 
 namespace net_il_mio_fotoalbum.Controllers
@@ -114,6 +115,98 @@ namespace net_il_mio_fotoalbum.Controllers
             }
 
 
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            using(_db)
+            {
+                Photo? foundedPhoto = _db.Photos.Where(photo => photo.Id == id).Include(photo => photo.Categories).FirstOrDefault();
+
+                if(foundedPhoto != null)
+                {
+                    List<Category> categories = _db.Categories.ToList();
+                    List<SelectListItem> selectedCategories = new List<SelectListItem>();
+                    foreach(Category category in categories)
+                    {
+                        selectedCategories.Add(new SelectListItem
+                        {
+                            Text = category.Name,
+                            Value = category.Id.ToString(),
+                            Selected = foundedPhoto.Categories.Any(x => x.Id == category.Id)
+                        });
+                    }
+
+                    PhotoFormModel photoModel = new PhotoFormModel()
+                    {
+                        Photos = foundedPhoto,
+                        Categories = selectedCategories
+                    };
+
+                    return View("Edit", photoModel);
+                }
+                else
+                {
+                    return RedirectToAction("Index");
+                }      
+            }
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id,PhotoFormModel editPhoto)
+        {
+            if(!ModelState.IsValid)
+            {
+                using(_db)
+                {
+                    List<Category> dbCategories = _db.Categories.ToList();
+                    List<SelectListItem> listSelectCategory = new List<SelectListItem>();
+                    foreach(Category category in dbCategories)
+                    {
+                        listSelectCategory.Add(new SelectListItem
+                        {
+                            Text = category.Name,
+                            Value = category.Id.ToString(),
+                        });
+                    }
+                    editPhoto.Categories = listSelectCategory;
+                    return View("Edit", editPhoto);
+                }
+            }
+
+            Photo? dbPhoto = _db.Photos.Where(photo => photo.Id == id).Include(photo => photo.Categories).FirstOrDefault();
+            if(dbPhoto != null)
+            {
+                MemoryStream stream = new MemoryStream();
+                editPhoto.ImageFile.CopyTo(stream);
+                editPhoto.Photos.Image = stream.ToArray();
+
+                if(editPhoto.SelectedCategories != null)
+                {
+                    dbPhoto.Categories.Clear();
+                    foreach(string selectCategory in editPhoto.SelectedCategories)
+                    {
+                        int categoryId = int.Parse(selectCategory);
+                        Category newCategory = _db.Categories.Where(category => category.Id == categoryId).FirstOrDefault();
+                        dbPhoto.Categories.Add(newCategory);
+                    }
+                }
+
+                dbPhoto.Title = editPhoto.Photos.Title;
+                dbPhoto.Description = editPhoto.Photos.Description;
+                dbPhoto.Image = editPhoto.Photos.Image;
+                dbPhoto.Visible = editPhoto.Photos.Visible;
+
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
         }
     }
 }
