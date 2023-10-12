ff4de7ad93ce9cfc0b08323b564cc857edd128cf
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-10-12 14:29:09 +0200

    Creazione stile pagina front-end

---
+++ fotoalbum-front/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import HeaderApp from './components/HeaderApp.vue';
+export default{
+  name: 'App',
+  components: {
+    HeaderApp
+  }
+}
+</script>
+
+<template>
+  <HeaderApp></HeaderApp>
+</template>
+
+<style scoped>
+</style>
---
+++ fotoalbum-front/src/components/HeaderApp.vue
@@ -0,0 +1,43 @@
+<template>
+    <header>
+        <div class="d-flex justify-content-between align-items-center h-100 container">
+            <div><img src="../../public/logo-fotoalbum.jpg" alt="Logo" class="img-fluid"></div>
+            <ul class="list-unstyled d-flex align-items-center gap-3 m-0">
+                <li>Home</li>
+                <li>Chi siamo</li>
+                <li>Contatti</li>
+                <li>Dashboard</li>
+            </ul>
+            <div>
+                <input type="text" name="filter" placeholder="Filtra le foto per nome" class="form-control">
+            </div>
+        </div>
+    </header>
+</template>
+<script>
+export default {
+    name: 'HeaderApp',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+header {
+    height: 85px;
+    background-color: white;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 2;
+    border-bottom: 1px solid lightgrey;
+    div{
+        img{
+            height: 75px;
+        }
+    }
+}
+</style>
